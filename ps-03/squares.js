8e8ea026746f6b7619f06e87c403c56c7999489f
millerniyah <miller.n@husky.neu.edu>
2016-10-05 00:09:09 -0400

    lab

--- ps-03/squares.js
+++ ps-03/squares.js
@@ -1,22 +1,12 @@
-<script type="text/javascript">
 
-  var canvas = document.getElementById('canvas');
-  var drawingPad = canvas.getContext('2d');
+    var canvas = document.getElementById('canvas');
+    var drawingPad = canvas.getContext('2d');
 
-  console.log(canvas);
-  console.log(drawingPad);
+    console.log(canvas);
+    console.log(drawingPad);
 
-  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-  drawingPad.fillRect(0, 0, 20, 20);
+    drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+    drawingPad.fillRect(0, 0, 20, 20);
 
-  drawingPad.fillStyle = "hsla(0,10%,60%,1)";
-  drawingPad.fillRect(40, 40, 20, 20);
-
-  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-  drawingPad.fillRect(0, 0, 20, 20);
-
-  drawingPad.fillStyle = "hsla(0,10%,60%,1)";
-  drawingPad.fillRect(80, 60, 40, 20);
-
-
-</script>
+    drawingPad.fillStyle = "hsla(0,10%,60%,1)";
+    drawingPad.fillRect(40, 40, 20, 20);
