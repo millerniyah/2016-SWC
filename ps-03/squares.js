42b834ed841d3326db9c188e5ce2891b6e38f1e5
millerniyah <miller.n@husky.neu.edu>
2016-10-04 23:11:15 -0400

    idk

---
+++ ps-03/squares.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  var canvas = document.getElementById('canvas');
+  var drawingPad = canvas.getContext('2d');
+
+  console.log(canvas);
+  console.log(drawingPad);
+
+  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+  drawingPad.fillRect(0, 0, 20, 20);
+
+  drawingPad.fillStyle = "hsla(0,10%,60%,1)";
+  drawingPad.fillRect(40, 40, 20, 20);
+
+
+</script>
