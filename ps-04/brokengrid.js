4d478b8832ea1d6f892c5abdc6e73b0e4af50114
millerniyah <miller.n@husky.neu.edu>
2016-10-07 17:00:50 -0400

    trying to connect

--- ps-04/brokengrid.js
+++ ps-04/brokengrid.js
@@ -1,6 +1,4 @@
-<script type="text/javascript">
   var canvas = document.getElementById('canvas');
   var drawingPad = canvas.getContext('2d');
   drawingPad.fillStyle = "hsla(0,10%,10%,1)";
   drawingPad.fillRect(0, 0, 20, 20);
-</script>
