4108c8312d2210ddd22d3d2619273ab85e456290
millerniyah <miller.n@husky.neu.edu>
2016-10-10 22:44:36 -0400

    fadgadf

--- ps-04/brokengrid.js
+++ ps-04/brokengrid.js
@@ -1,4 +1,4 @@
-  var canvas = document.getElementById('canvas');
-  var drawingPad = canvas.getContext('2d');
-  drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-  drawingPad.fillRect(0, 0, 20, 20);
+var canvas = document.getElementById('canvas');
+var drawingPad = canvas.getContext('2d');
+drawingPad.fillStyle = "hsla(0,10%,10%,1)";
+drawingPad.fillRect(0, 0, 20, 20);
