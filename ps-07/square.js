6db028782dbe151969470de467bcc07b114168fc
millerniyah <miller.n@husky.neu.edu>
2016-11-01 19:47:41 -0400

    f

--- ps-07/square.js
+++ ps-07/square.js
@@ -17,12 +17,6 @@ function drawlines(){
 
 drawlines();
 
-//draw lines as as the mouse hovered over the lines
+//draw lines as as the mouse is hovered over the lines
 //the lines are supposed to change size as the mouse is hovered over the canvas
-//as lines reappear, they change color in gradient form
-function mouseOver() {
-   var element = document.getElementById("canvas");
-   var top = parseInt(element.style.top) || 0;
-   top = top -5;
-   element.style.top = top + "px";
- }
+//as lines reappear, they should change color randomly
