d45caaf4158d99ccfc198c195f8ef9f896c0f88b
millerniyah <miller.n@husky.neu.edu>
2016-11-29 19:44:50 -0500

    djf

--- ps-09/snowstorm.js
+++ ps-09/snowstorm.js
@@ -3,11 +3,3 @@ function init(){
   snow.src="http://www.clipartkid.com/images/54/animated-clip-art-snow-falling-index-of-MgYceV-clipart.gif"
   window.requestAnimationFrame(draw);
 }
-
-ctx.beginPath();
-ctx.moveTo(150, 20);
-ctx.arcTo(150,100,50,20,30);
-ctx.stroke();
-
-ctx.fillStyle = 'white';
-ctx.fillRect(150, 20, 10, 10);
