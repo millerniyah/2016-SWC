c8100acf82fa44b7791eec25e53a9d3b4b9e09c2
millerniyah <miller.n@husky.neu.edu>
2016-12-06 19:43:15 -0500

    bar graph

--- ps-10/graph.js
+++ ps-10/graph.js
@@ -1,37 +1,32 @@
-// A simple scatter plot
+// A simple bar chart
 var width = 500;
-var height = 500;
+var height =500;
 
-var margin = {
-	top: 25,
-  left: 25,
-  right: 25,
-  bottom: 25
-};
+var margin = {top: 25,
+		  left:25,
+		  right: 25,
+		  bottom:25};
 
 var svg = d3.select("body")
-	.append("svg")
-  .attr("width", width)
-  .attr("height", height);
+  .append("svg")
+  .attr("width",width)
+  .attr("height",height);
 
-  //Declare data
-  var data = [
-  	{x: 3, y :2},
-    {x: 4, y: 8},
-    {x: 5, y: 8},
-  	{x: 8, y: 4},
-  	{x: 9, y: 6},
-  	{x: 11, y:8}
-  ];
+// Generate data
+
+var data =
+		[{x:"White", y:450},
+		{x:"Hispanic", y:831},
+		{x:"African American", y:2306},];
 
 // Create scales
-var xScale = d3.scaleLinear()
-	.domain([0,12])
-  .range([margin.left, width - margin.right]);
+var xScale = d3.scaleBand()
+	.domain(["White","Hispanic","African American",])
+  	.rangeRound([margin.left, width - margin.right]);
 
 var yScale = d3.scaleLinear()
-	.domain([0,12])
-  .range([height - margin.bottom, margin.top]);
+	.domain([0,100,000])
+  	.range([height-margin.bottom, margin.top]);
 
 // Draw axes
 var xAxis = svg.append("g")
@@ -42,15 +37,21 @@ var yAxis = svg.append("g")
 	.attr("transform","translate(" + margin.left + ",0)")
 	.call(d3.axisLeft().scale(yScale));
 
-//Draw circles for scatter plot
-var circles = svg.selectAll("circle")
+// Draw bars for bar chart
+var barWidth = 40;
+var bars = svg.selectAll("rect")
 	.data(data)
-  .enter()
-  .append("circle")
-  	.attr("cx", function(d) {return xScale(d.x);})
-    .attr("cy", function(d) {return yScale(d.y);})
-    .attr("r", 3);
+	.enter()
+	.append("rect")
+      	.attr("x", function(d) {
+		  return xScale(d.x) + xScale.bandwidth()/2 - barWidth/2;
+		})
+		.attr("y", function(d) { return yScale(d.y); })
+		.attr("width", barWidth)
+		.attr("height", function(d) {
+		  return height - margin.bottom - yScale(d.y);
+		})
+    .attr("fill","steelblue");
 
     //http://www.vanityfair.com/news/2016/07/data-police-racial-bias
     //http://mappingpoliceviolence.org/
-    
